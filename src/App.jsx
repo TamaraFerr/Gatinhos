bcd30826d4fdba92fb4d9204c3434f3a102172aa
TamaraFerr <tamarablacksmith@hotmail.com>
2025-03-06 17:04:53 -0300

    FEAT: instaling some dependencies and simples layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Gatinhos from "../src/assets/gatinhos.png"
+
 function App() {
   return (
-    <>
+    <div>
       <h1>Bem-vindo ao meu app de gerar gatinhos fofos!!</h1>
+      <p>Aqui, você podderá gerar fotos incríveis e super fofas de gatinhos!</p>
 
-      <button>Gerar gato</button>
-    </>
+      <img src={Gatinhos} alt="gatinhos"/>
+      <button class="bg-sky-500 hover:bg-sky-700">Gerar gato</button>
+    </div>
   )
 }
 
