15661efa6abf6fc617b127cfd2a779deb6d5fba0
TamaraFerr <tamarablacksmith@hotmail.com>
2025-03-07 19:27:18 -0300

    FIX: reajusted tailwind configs

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
       <h1>Bem-vindo ao meu app de gerar gatinhos fofos!!</h1>
       <p>Aqui, você podderá gerar fotos incríveis e super fofas de gatinhos!</p>
 
-      <img src={Gatinhos} alt="gatinhos"/>
-      <button class="bg-sky-500 hover:bg-sky-700">Gerar gato</button>
+      <img className="size-100" src={Gatinhos} alt="gatinhos"/>
+      <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Gerar gato</button>
     </div>
   )
 }
