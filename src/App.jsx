bfd5249fd76e057e353ea435e885b4bd6e8b56eb
TamaraFerr <tamarablacksmith@hotmail.com>
2025-03-10 19:19:21 -0300

    FEAT: add customization(40% done)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Gatinhos from "../src/assets/gatinhos.png"
 
 function App() {
   return (
-    <div>
-      <h1>Bem-vindo ao meu app de gerar gatinhos fofos!!</h1>
-      <p>Aqui, você podderá gerar fotos incríveis e super fofas de gatinhos!</p>
+    <div className="box-border flex flex-col my-10 bg-fuchsia-50">
+      <h1 className="block mx-auto font-sans font-bold text-2xl text-center	">Bem-vindo ao meu app de gerar gatinhos fofos!!</h1>
+      <p className="block mx-auto font-sans font-medium text-base text-center	mt-5 mb-10">Aqui, você podderá gerar fotos incríveis e super fofas de gatinhos!</p>
 
-      <img className="size-100" src={Gatinhos} alt="gatinhos"/>
-      <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Gerar gato</button>
+      <img className="block mx-auto w-1/4 rounded-full mb-20" src={Gatinhos} alt="gatinhos"/>
+      <button className="block mx-auto px-4 py-1  bg-white text-base text-pink-400 font-semibold rounded-full border border-pink-200 hover:text-white hover:bg-pink-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Gerar gatinho</button>
     </div>
   )
 }
